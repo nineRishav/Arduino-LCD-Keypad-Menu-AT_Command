1350833181b5f98ff5ac03dccc8ebf881a32ac9f
Rishav KUMAR <13173745+oddRishav@users.noreply.github.com>
2019-09-09 01:25:12 +0530

    Serial Monitor for Char & Num - Baudrate -9600

---
+++ Serial Monitor, 9600/Serial_Input_of_character/Serial_Input_of_character.ino
@@ -0,0 +1,35 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Serial Monitor Started");
+}
+
+char rx_byte = 0;
+String rx_str = "";
+char value[3];
+uint8_t i=0;
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    rx_byte = Serial.read();
+    if(rx_byte == ',')
+    {
+      rx_str.toCharArray(value[i],3);
+      memset(rx_str,'\0',sizeof(rx_str));
+      Serial.println(value[i]);
+      i++;
+    }
+    else if (rx_byte != '\n')
+      rx_str += rx_byte;
+    else
+    {
+      // end of string
+      Serial.print("Your DATA : ");
+      Serial.println(rx_str);
+      rx_str = "";
+      Serial.println("");
+    }
+  }
+}
+//3088 byte used
+
---
+++ Serial Monitor, 9600/Serial_Input_of_number/Serial_Input_of_number.ino
@@ -0,0 +1,46 @@
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Serial Monitor started...");
+}
+
+char rx_byte = 0;
+String rx_str = "";
+boolean not_number = false;
+uint8_t result;
+
+void loop()
+{
+  rxSerialData();
+}
+
+void rxSerialData()
+{
+  if (Serial.available() > 0)
+  {
+    rx_byte = Serial.read();
+    if ((rx_byte >= '0') && (rx_byte <= '9'))
+      rx_str += rx_byte;
+
+    else if (rx_byte == '\n')
+    {
+      // end of string
+      if (not_number)
+        Serial.println("Not a number");
+      else
+      {
+        result = rx_str.toInt();
+        Serial.print("Your DATA : ");
+        Serial.print(rx_str);
+        Serial.println("");
+      }
+      not_number = false;
+      rx_str = "";
+    }
+    else
+    {
+      not_number = true;    // flag
+    }
+  }
+
+}
